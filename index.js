3bbc48ca9763a7730536b2efe8e591256bd3030b
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-04-06 15:46:44 +0500

    feat:main.jus

--- index.js
+++ index.js
@@ -34,3 +34,120 @@ if (seriesDB.count < 5) {
 }
 
 console.log(seriesDB)
+
+// let a = 10
+// let i = 1
+// let sum = 0
+// while (i<=a){
+//   if(i%2==0){
+//     sum += i
+//   }
+//   i += 1
+
+// }
+// console.log(sum);
+
+// let a = 10
+// let i = 1
+// let sum = 0
+// do {
+//     if(i%2==0){
+//     sum += i
+//   }
+//   i += 1
+
+// } while (i<=a);
+// console.log(sum);
+
+
+// const a = prompt('1dan 7gacha son kiriting: ')
+// switch(a){
+//   case '1':
+//     alert("dushanba");
+//     break;
+//   case '2':
+//     alert("seshanba");
+//     break;
+//   case '3':
+//     alert("chorshba");
+//     break;
+//   case '4':
+//     alert("payshanba");
+//     break;
+//   case '5':
+//     alert("juma");
+//     break;
+//   case '6':
+//     alert("shanba");
+//     break;
+//   case '7':
+//     alert("yakshanba");
+//     break;
+//     default:
+//       alert('hato')
+// }
+
+// let a = +prompt("")
+// let c = prompt("amal kiriting")
+// let b = +prompt("")
+
+// function func(num ,num1 ,c){
+//   switch (c){
+//     case "+":
+//       alert(num+num1)
+//       break;
+//       case "-":
+//       alert(num-num1)
+//       break;
+//       case "*":
+//       alert(num*num1)
+//       break;
+//       case "/":
+//       alert(num/num1)
+//       break;
+//   }
+// }
+// func(a ,b ,c )
+
+// let c = eval(prompt("amal kiriting"))
+// alert(c)
+
+// function yigindi(num ,num1){
+//   let sum = 0
+  
+//   for (num;num<=num1;num++){
+//     sum += num
+//   }
+
+//   return sum;
+// }
+// yigindi(1,10) 
+
+// const plase = confirm("gkjnfkjnbfkjg6")
+// console.log(plase )
+
+
+// let obj = {
+//     name : "Nurbek",
+//     lastname : "Abduaxatov",
+//     age : 17,
+//     gmail : "abduaxatov007@gmail.com",
+//     adress : {
+//         city : "andijan",
+//         street : "home streat" ,
+//         nomer : "1"
+//     },
+//     hobby : ["reading","geming","sport"],
+//     func : function(){},
+// }
+// for (let key in obj.adress){
+//     console.log(obj.adress[key]);
+// }
+
+
+// let obj ={
+//     func : function(){
+//         console.log("hello");
+//     },
+// }
+// obj.func()
